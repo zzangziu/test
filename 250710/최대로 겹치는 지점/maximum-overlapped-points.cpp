c8ebc0a5b2850b9203630d285d605d7989edd8ae
Jiwoo-Yun <137812024+zzangziu@users.noreply.github.com>
2025-07-10 20:26:41 +0900

    Title: 최대로 겹치는 지점, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250710/최대로 겹치는 지점/maximum-overlapped-points.cpp
+++ 250710/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -16,7 +16,7 @@ int main() {
     int cnt[105] = {};
     int max = 0;
     for(int i = 0; i < n; i++) {
-        for(int j = x1[i]; j <= x2[i]+1; j++) {
+        for(int j = x1[i]; j <= x2[i]; j++) {
             cnt[j]++;
             if(max < cnt[j]) max = cnt[j];
         }
