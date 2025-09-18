2d068fd25bfc73a3af106ae35542f7cf4df67077
Jiwoo-Yun <137812024+zzangziu@users.noreply.github.com>
2025-09-18 21:04:34 +0900

    Title: 그래프 탐색, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250918/그래프 탐색/graph-traversal.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int cnt=0;
+int from[10000], to[10000];
+bool graph[10000][10000];
+bool visited[10000];
+
+void DFS(int vertex) {
+    for(int cur_v = 1; cur_v <= n; cur_v++) {
+        if(graph[vertex][cur_v] && !visited[cur_v]) {
+            cnt++;
+            visited[cur_v] = true;
+            DFS(cur_v);
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int from, to;
+        cin >> from >> to;
+        graph[from][to]=true;
+        graph[to][from]=true;
+    }
+
+    DFS(1);
+    cout << --cnt;
+
+    // Please write your code here.
+
+    return 0;
+}
