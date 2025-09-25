0e48c2c969eb98f30adade38faca28808b54aeb9
Jiwoo-Yun <137812024+zzangziu@users.noreply.github.com>
2025-09-25 14:40:58 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 10ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250925/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+using namespace std;
+
+int n, m;
+bool a[100][100] = {false};
+bool visited[100][100] = {false};
+queue<pair<int,int>> q;
+int x, y;
+
+bool canGo (int x, int y) {
+    if(x < 0 || y < 0 || x > n || y > m) return false;
+    if(visited[x][y] || !a[x][y]) return false;
+    return true;
+}
+
+void BFS() {
+    while(!q.empty()) {
+        pair<int, int> pos = q.front();
+        q.pop();
+
+        x = pos.first;
+        y = pos.second;
+        if(!a[x][y]) visited[x][y];
+
+        if(canGo(x+1, y)) {
+            visited[x+1][y]= true;
+            q.push(make_pair(x+1, y));
+        }
+        if(canGo(x-1, y)) {
+            visited[x-1][y]= true;
+            q.push(make_pair(x-1, y));
+        }
+        if(canGo(x, y+1)) {
+            visited[x][y+1]= true;
+            q.push(make_pair(x, y+1));
+        }
+        if(canGo(x, y-1)) {
+            visited[x][y-1]= true;
+            q.push(make_pair(x, y-1));
+        }
+    }
+}
+
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    // Please write your code here.
+    for(int i = 0; i<n; i++) {
+        for(int j = 0; j<m; j++) {
+            if(!visited[i][j] && a[i][j]) q.push(make_pair(i, j));
+        }
+    }
+    BFS();
+    if(visited[n-1][m-1]) cout << "1";
+    else cout << "0";
+
+    return 0;
+}
