2bf87f990abe62e912c012f5aabf073acf7e74a8
Jiwoo-Yun <137812024+zzangziu@users.noreply.github.com>
2025-09-29 19:12:16 +0900

    Title: 계단 오르기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250929/계단 오르기/climbing-stairs.cpp
+++ 250929/계단 오르기/climbing-stairs.cpp
@@ -9,11 +9,11 @@ int main() {
 
     // Please write your code here.
     int dp[1001];
-    dp[1] = 0;
+
     dp[2] = 1;
     dp[3] = 1;
     for(int i = 4; i <= n; i++) {
-        dp[i] = min(dp[i-2]+1, dp[i-3]+1);
+        dp[i] = (dp[i-2] + dp[i-3]) % 10007;
     }
 
     cout << dp[n];
