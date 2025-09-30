d01405b642fa311bdfb0ab18a834e38cad49a0fc
Jiwoo-Yun <137812024+zzangziu@users.noreply.github.com>
2025-09-30 11:12:20 +0900

    Title: 사각형 채우기 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250930/사각형 채우기 2/rectangle-fill-2.cpp
+++ 250930/사각형 채우기 2/rectangle-fill-2.cpp
@@ -13,7 +13,7 @@ int main() {
     dp[1] = 1;
     dp[2] = 3;
     for(int i = 3; i<= n; i++) {
-        dp[i] = dp[i-1] + dp[i-2]*2;
+        dp[i] = (dp[i-1] + dp[i-2]*2)%10007;
     }
     
     cout << dp[n];
