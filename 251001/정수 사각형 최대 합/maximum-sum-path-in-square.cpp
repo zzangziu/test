9e742d817273190f38fb4a78e7462a8d05380c40
Jiwoo-Yun <137812024+zzangziu@users.noreply.github.com>
2025-10-01 16:20:33 +0900

    Title: 정수 사각형 최대 합, Time: 11ms, Memory: 0MB, Status: Passed - Codetree

--- 251001/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
+++ 251001/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -6,15 +6,6 @@ int n;
 int grid[100][100];
 int res[100][100] = {0};
 
-
-void compare(int x, int y) {
-    if(x >= n || y >= n) return;
-
-    if(x != 0 && y != 0) res[x][y] = max(res[x-1][y], res[x][y-1])+grid[x][y];
-    compare(x+1, y);
-    compare(x, y+1);
-}
-
 int main() {
     cin >> n;
 
@@ -30,7 +21,11 @@ int main() {
         res[0][i] = res[0][i-1] + grid[0][i];
     }
 
-    compare(1, 1);
+    for(int i =1; i<n;i++) {
+        for(int j =1; j<n; j++) {
+            res[i][j] = max(res[i-1][j], res[i][j-1])+grid[i][j];
+        }
+    }
 
     cout << res[n-1][n-1];
 
