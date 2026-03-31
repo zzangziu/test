2068d2165fe4e8057f4d530b5b50af67b00ec4d9
Jiwoo-Yun <137812024+zzangziu@users.noreply.github.com>
2026-03-31 14:29:47 +0900

    Title: 행복한 수열의 개수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260331/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 260331/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
-    <vector<vector<int>> v(n, vector<int>(n));
+    vector<vector<int>> v(n, vector<int>(n));
     for(int i=0; i<n; i++) {
         for(int j = 0; j<n; j++) {
              cin >> v[i][j];
@@ -13,17 +13,25 @@ int main() {
     }
     int happy = 0;
     for(int i=0; i<n; i++) {
-        int count = 0;
-        for(int j=0; j<n; j++) {
+        int count = 1;
+        for(int j=0; j<n-1; j++) {
             if(v[i][j] == v[i][j+1]) count++;
-            if(m <= count) happy++;
+            else count = 1;
+            if(m <= count) {
+                happy++;
+                break;
+            }
         }
     }
     for(int i=0; i<n; i++) {
-        int count = 0;
-        for(int j=0; j<n; j++) {
-            if(v[j][i] == v[j][i+1]) count++;
-            if(m <= count) happy++;
+        int count = 1;
+        for(int j=0; j<n-1; j++) {
+            if(v[j][i] == v[j+1][i]) count++;
+            else count=1;
+            if(m <= count) {
+                happy++;
+                break;
+            }
         }
     }
     cout << happy;
