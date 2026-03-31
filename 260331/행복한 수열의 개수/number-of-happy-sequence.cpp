dbdef13254c89bc5ce3a832413b6de7989b7eebf
Jiwoo-Yun <137812024+zzangziu@users.noreply.github.com>
2026-03-31 14:32:40 +0900

    Title: 행복한 수열의 개수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260331/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 260331/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -8,7 +8,7 @@ int main() {
     vector<vector<int>> v(n, vector<int>(n));
     if(m==1) {
         cout << n*n;
-        return;
+        return 0;
     }
     for(int i=0; i<n; i++) {
         for(int j = 0; j<n; j++) {
