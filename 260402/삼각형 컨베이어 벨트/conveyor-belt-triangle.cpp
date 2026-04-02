3baacf1cef3bc770bd91e013754635c0d40334b6
Jiwoo-Yun <137812024+zzangziu@users.noreply.github.com>
2026-04-02 18:46:11 +0900

    Title: 삼각형 컨베이어 벨트, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260402/삼각형 컨베이어 벨트/conveyor-belt-triangle.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, t;
+    cin >> n >> t;
+    vector<int> v(3*n+1);
+    for(int i = 0; i<n*3; i++) {
+        cin >> v[i];
+    }
+    t %= 9;
+
+    for(int i =0; i<t; i++) {
+        for(int j=n*3; j>=0; j--) {
+            v[j] = v[j-1];
+        }
+        v[0] = v[3*n];
+    }
+
+    for(int i = 0; i<v.size() - 1; i++) {
+        if(i == n || i == 2*n) {
+            cout << '\n';
+        }
+        cout << v[i] << ' ';
+    }
+    return 0;
+}
